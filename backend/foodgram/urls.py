8fe3d41ae0b6d7ac2cffcbf4c489f62de0791bab
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2022-05-27 22:19:27 +0300

    Fix Redoc

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        'api/docs/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
     path('api/', include('recipes.urls', namespace='recipes')),
 ]
