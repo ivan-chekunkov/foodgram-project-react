ee9db6f4a9ecbb5abce3582ecc15b3060d888a4b
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2022-05-29 13:36:21 +0300

    Fix Redoc

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': os.getenv(
@@ -90,12 +90,6 @@ DATABASES = {
         'PORT': os.getenv(
             'DB_PORT',
             default='5432'),
-    }
-}"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
